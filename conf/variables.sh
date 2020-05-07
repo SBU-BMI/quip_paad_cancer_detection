d94e4efb0cfb333cd24ebd04ad3e1a7172689e3c
Han Le <32404085+hansbu@users.noreply.github.com>
2020-05-07 11:29:34 -0400

    auto detect base directory

--- conf/variables.sh
+++ conf/variables.sh
@@ -9,7 +9,8 @@ MONGODB_PORT=27017
 HEATMAP_VERSION=cancer-paad
 
 # Base directory
-BASE_DIR=/data02/shared/hanle/quip_paad_cancer_detection_seer_one_image
+# BASE_DIR=/data02/shared/hanle/quip_paad_cancer_detection_seer_one_image
+BASE_DIR="$(dirname "$PWD")"        # auto detect base directory
 
 # The username you want to download heatmaps from
 #USERNAME=olcabasturk@gmail.com
