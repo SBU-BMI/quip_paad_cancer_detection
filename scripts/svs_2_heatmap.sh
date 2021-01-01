5ac6693118c6235a50125a63d5dca4b82f1476e1
Kurc <tkurc@gpu003.cm.cluster>
2021-01-01 13:44:20 -0500

    Check and create folders.

--- scripts/svs_2_heatmap.sh
+++ scripts/svs_2_heatmap.sh
@@ -5,11 +5,11 @@ source ./conf/variables.sh
 
 out_folders="heatmap_jsons heatmap_txt json log patch-level-color patch-level-lym  patch-level-merged  patch-level-nec"
 for i in ${out_folders}; do
-	if [[ -d "${OUT_DIR}/$i" ]]; then
+	if [ ! -d "${OUT_DIR}/$i" ]; then
 		mkdir -p ${OUT_DIR}/$i
 	fi
 done
-if [[ -d "${DATA_DIR}/patches" ]]; then
+if [ ! -d "${DATA_DIR}/patches" ]; then
 	mkdir -p ${DATA_DIR}/patches;
 fi
 wait;
