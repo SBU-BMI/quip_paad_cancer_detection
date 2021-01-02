52251d7dc0400e7e9e1fe4b1ccb8eb88043c20df
Kurc <tkurc@login01.cm.cluster>
2021-01-02 13:42:26 -0500

    Updated data location.

--- conf/variables.sh
+++ conf/variables.sh
@@ -13,7 +13,7 @@ fi
 
 # Base data and output directories
 export BASE_DIR=/root/quip_paad_cancer_detection
-export DATA_DIR=${BASE_DIR}/data
+export DATA_DIR=/data
 export OUT_DIR=${DATA_DIR}/output
 
 # Prediction folders
